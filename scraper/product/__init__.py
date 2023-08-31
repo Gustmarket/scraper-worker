9a0a294e10e23c728de5c60dd7c54b0cdaaf0451
Victor Balan <victor.balan93@gmail.com>
2023-08-31 19:10:04 +0100

    test

--- scraper/product/__init__.py
+++ scraper/product/__init__.py
@@ -1,3 +1,4 @@
+import hashlib
 import traceback
 from datetime import datetime, timedelta
 
@@ -5,7 +6,6 @@ import pymongo
 from pymongo import ReturnDocument
 
 import database
-from scraper.product.base import route_scraper
 from scraper.product.handlers.coronation_industries import coronation_industries
 from scraper.product.handlers.flysurf import flysurf
 from scraper.product.handlers.glissattitude import glissattitude
@@ -18,27 +18,63 @@ from scraper.product.handlers.zephcontrol import zephcontrol
 from scraper.utils import get_main_domain
 
 product_route_handlers = {
-    'you-love-it.eu': route_scraper('you-love-it.eu', you_love_it),
-    'coronation-industries.de': route_scraper('coronation-industries.de', coronation_industries),
-    'kitemana.com': route_scraper('kitemana.com', kitemana),
-    'glissattitude.com': route_scraper('glissattitude.com', glissattitude),
-    'surfpirates.de': route_scraper('surfpirates.de', surfpirates),
-    'magasin-glissevolution.com': route_scraper('magasin-glissevolution.com', magasin_glissevolution),
-    'zephcontrol.com': route_scraper('zephcontrol.com', zephcontrol),
-    'side-shore.com': route_scraper('side-shore.com', side_shore),
-    'flysurf.com': route_scraper('flysurf.com', flysurf)
+    'you-love-it.eu': you_love_it,
+    'coronation-industries.de': coronation_industries,
+    'kitemana.com': kitemana,
+    'glissattitude.com': glissattitude,
+    'surfpirates.de': surfpirates,
+    'magasin-glissevolution.com': magasin_glissevolution,
+    'zephcontrol.com': zephcontrol,
+    'side-shore.com': side_shore,
+    'flysurf.com': flysurf
 }
 
 
-async def product_route(url, user_data, playwright_context):
+async def product_route(product_url, playwright_context):
+    url = product_url['url']
+    user_data = {"hash": product_url['hash']}
+    print(f'Scraping {url} ...')
     if user_data is None or user_data.get('hash') is None:
         print('no hash present on product')
         return
-    domain_handler = product_route_handlers.get(get_main_domain(url))
+    source = get_main_domain(url)
+    domain_handler = product_route_handlers.get(source)
     if domain_handler is None:
-        print('no domain handler')
+        print('no domain handler for', source)
         return
-    await domain_handler(url=url, user_data=user_data, playwright_context=playwright_context)
+    url_hash = user_data.get('hash')
+    result = None
+    try:
+        data = await domain_handler(url=url, user_data=user_data, playwright_context=playwright_context)
+        if data is None:
+            result = {
+                'source': source,
+                'hash': url_hash,
+                'url': url,
+                'loaded_url': url,
+                'type': "OUT_OF_STOCK"}
+        else:
+            (product, product_type) = data
+            result = {
+                'source': source,
+                'hash': url_hash,
+                'url': url,
+                'loaded_url': url,
+                'item': product,
+                'type': product_type}
+    except Exception as e:
+        traceback.print_exc()
+        result = {
+            'source': source,
+            'hash': url_hash,
+            'url': url,
+            'loaded_url': url,
+            'error': str(e),
+            'type': "ERROR"}
+    finally:
+        if result.get('hash') is None:
+            result['hash'] = hashlib.sha256(url.encode("UTF-8")).hexdigest()
+        database.get_model('raw_items').update_one({'hash': result['hash']}, {'$set': result}, upsert=True)
 
 
 async def get_one_expired_product_url_and_update(playwright_context):
@@ -61,10 +97,7 @@ async def get_one_expired_product_url_and_update(playwright_context):
         return None
 
     try:
-        url = product_url['url']
-        user_data = {"hash": product_url['hash']}
-        print(f'Scraping {url} ...')
-        await product_route(url, user_data, playwright_context)
+        await product_route(product_url, playwright_context)
     except Exception as e:
         traceback.print_exc()
         product_urls_model.update_one({'_id': product_url['_id']},
