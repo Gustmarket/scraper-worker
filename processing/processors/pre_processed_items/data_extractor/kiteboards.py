dad0a065a94a5367fae47109c2cdf55a6342679e
Victor Balan <victor.balan93@gmail.com>
2024-10-23 15:26:44 +0300

    kb size

--- processing/processors/pre_processed_items/data_extractor/kiteboards.py
+++ processing/processors/pre_processed_items/data_extractor/kiteboards.py
@@ -1,63 +1,59 @@
 import re
 
-from processing.data.cleanup import replace_string_ignore_case
 from processing.data.utils import uniq_filter_none, flatten_list
+
+def extract_and_cleanup_surfboard_size(raw):
+    surfboard_pattern = r"(\d+)'\s*(\d+(?:\.\d+)?)?\"?"  # Matches formats like 6'3, 5'7.5, 6' 3", or 6'  3
+
+    surfboard_match = re.search(surfboard_pattern, raw)
+    if surfboard_match:
+        feet = int(surfboard_match.group(1))
+        inches = float(surfboard_match.group(2).replace(',', '.')) if surfboard_match.group(2) else 0
+
+        cleaned_name = re.sub(surfboard_pattern, '', raw).strip()
+        return cleaned_name, f"{feet}'{inches:.1f}" if inches else f"{feet}'"
+
+    return raw, None
+
+def extract_and_cleanup_twintip_size(raw):
+    twintip_length_pattern = r'\b(\d{3})\b'
+    twintip_width_pattern = r'\b(\d{3})\b'
+    twintip_full_size_pattern = r'\b(\d{3})\s*x\s*(\d{2})\b'
+    twintip_length_match = re.search(twintip_length_pattern, raw)
+    if twintip_length_match:
+        twintip_length = float(twintip_length_match.group(1).replace(',', '.'))
+        cleaned_name = re.sub(twintip_full_size_pattern, '', raw).strip()
+        cleaned_name = re.sub(twintip_length_pattern, '', cleaned_name).strip()
+        cleaned_name = re.sub(twintip_width_pattern, '', cleaned_name).strip()
+        return cleaned_name, twintip_length
+    return raw, None
 
 
 def extract_and_cleanup_kiteboard_size(name):
-    # Define the regular expression patterns for various size formats
-    twintip_pattern = r'(\d+(?:[.,]\d+)?)\s*(?:x|-)?\s*(\d+(?:[.,]\d+)?)?\s*(?:cm)?'
-    surfboard_pattern = r"(\d+)'(\d+(?:\.\d+)?)?"  # Matches formats like 6'3 or 5'7.5
+    cleaned_name, surfboard_size = extract_and_cleanup_surfboard_size(name)
+    if surfboard_size is not None:
+        return cleaned_name, surfboard_size
 
-    # Try to match twintip board size
-    twintip_match = re.search(twintip_pattern, name, re.IGNORECASE)
-    if twintip_match:
-        size1 = float(twintip_match.group(1).replace(',', '.'))
-        size2 = float(twintip_match.group(2).replace(',', '.')) if twintip_match.group(2) else 0
+    cleaned_name, twintip_size = extract_and_cleanup_twintip_size(name)
+    if twintip_size is not None:
+        return cleaned_name, twintip_size
 
-        board_size = max(size1, size2)
-        board_size = round(board_size)
-
-        cleaned_name = re.sub(twintip_pattern, '', name, flags=re.IGNORECASE).strip()
-
-        return cleaned_name, board_size
-
-    # Try to match surfboard size
-    surfboard_match = re.search(surfboard_pattern, name)
-    if surfboard_match:
-        feet = int(surfboard_match.group(1))
-        inches = float(surfboard_match.group(2)) if surfboard_match.group(2) else 0
-
-        board_size = f"{feet}'{inches:.1f}" if inches else f"{feet}'"
-
-        cleaned_name = re.sub(surfboard_pattern, '', name).strip()
-
-        return cleaned_name, board_size
-
-    # If no match found
     return name, None
 
 
 def map_kiteboard_size(variant_name, kv):
     size = kv.attributes.get('size', None)
-    # if size is None:
-    #     todo
-    #     variant_labels = kv.attributes.get('variant_labels', [])
-    #     if variant_labels is None:
-    #         variant_labels = []
-    #
-    #     size_variant_labels = filter_none(list(map(map_kite_variant_label_to_size_or_none, variant_labels)))
-    #     if len(size_variant_labels) > 0:
-    #         size = size_variant_labels[0]
-    # size = cleanup_size(size)
+    extracted_size = None
+    if size is not None:
+        _, extracted_size = extract_and_cleanup_kiteboard_size(size)
 
-    if size is None:
+    if extracted_size is None:
         name_variants = kv.name_variants
         if name_variants is None:
             name_variants = []
         name_variants = uniq_filter_none(flatten_list(name_variants + [variant_name]))
         for name_variant in name_variants:
-            _, size = extract_and_cleanup_kiteboard_size(name_variant)
-            if size is not None:
+            _, extracted_size = extract_and_cleanup_kiteboard_size(name_variant)
+            if extracted_size is not None:
                 break
-    return size
+    return extracted_size
